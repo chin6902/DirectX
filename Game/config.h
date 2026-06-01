8b882fe61f99d854f137e3e5bfe9e31dd6c68d25
chin6902 <chin6902@gmail.com>
2026-06-01 13:29:27 +0900

    Window Setup

---
+++ Game/config.h
@@ -0,0 +1,15 @@
+/*============================================================================
+Contents   :  [config.h]
+
+Author     : Chin Qing You
+LastUpdate : 2026/06/01
+-----------------------------------------------------------------------------
+Settings for the game
+============================================================================*/
+#ifndef CONFIG_H
+#define CONFIG_H
+
+inline constexpr unsigned int SCREEN_WIDTH = { 1600 };
+inline constexpr unsigned int SCREEN_HEIGHT = { 900 };
+
+#endif
--- Game/main.cpp
+++ Game/main.cpp
@@ -9,6 +9,11 @@ Windows program
 #include <SDKDDKVer.h>
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <algorithm>
+
+#include "config.h"
+
+using namespace std;
 
 static constexpr char WINDOW_CLASS[] = "GameWindow";
 static constexpr char TITLE[] = "Game";
@@ -18,8 +23,10 @@ PAINTSTRUCT ps;
 RECT rc;
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPSTR /*lpCmdLine*/, _In_ int nCmdShow)
 {
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
 	WNDCLASSEX wcex
 	{
 		.cbSize = sizeof(WNDCLASSEX),
@@ -35,7 +42,32 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	RegisterClassEx(&wcex);
 
-	HWND hWnd = CreateWindow(WINDOW_CLASS, TITLE, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	// window size
+	RECT window_rect = { .left = 0, .top = 0, .right = SCREEN_WIDTH, .bottom = SCREEN_HEIGHT };
+
+	//window style
+	constexpr DWORD WINDOW_STYLE = WS_OVERLAPPEDWINDOW & ~(WS_THICKFRAME | WS_MAXIMIZEBOX);
+
+	AdjustWindowRect(&window_rect, WINDOW_STYLE, FALSE);
+
+	// Calculate the actual window size after adjusting for the window style
+	const int WINDOW_WIDTH { window_rect.right - window_rect.left };
+	const int WINDOW_HEIGHT { window_rect.bottom - window_rect.top };
+
+	// Get the main display desktop resolution
+	int desktop_width = GetSystemMetrics(SM_CXSCREEN);
+	int desktop_height = GetSystemMetrics(SM_CYSCREEN);
+
+	// Center the window on the desktop
+	int window_x = std::max((desktop_width - WINDOW_WIDTH) / 2, 0);
+	int window_y = std::max((desktop_height - WINDOW_HEIGHT) / 2, 0);
+
+	HWND hWnd = CreateWindow(
+		WINDOW_CLASS, TITLE, WINDOW_STYLE,
+		window_x, window_y,
+		WINDOW_WIDTH, WINDOW_HEIGHT,
+		nullptr, nullptr, hInstance, nullptr
+	);
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
@@ -57,7 +89,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CLOSE: // ウィンドウを閉じるメッセージ
 		if (MessageBox(hWnd, "本当に終了してよろしいですか？",
-			"確認", MB_OKCANCEL | MB_DEFBUTTON2) == IDOK) 
+			"確認", MB_ICONEXCLAMATION | MB_OKCANCEL | MB_DEFBUTTON2) == IDOK) 
 		{
 			DestroyWindow(hWnd); // 指定のウィンドウにWM_DESTROYメッセージを送る
 		}
